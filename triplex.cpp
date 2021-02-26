31cd8fd6a59eb10b61cb3c4c112190502e78f3f3
HONEY <im@really.cool>
2021-02-26 14:51:47 -0800

    New version

--- triplex.cpp
+++ triplex.cpp
@@ -1,69 +1,50 @@
 #include <iostream>
 #include <ctime>
 
-bool PlayGame(int LevelDifficulty);
+bool PlayGame(int difficulty);
 
-int main()
-{
-    int Level = 1;
-
-    //Print Introduction
-    std::cout << "You are secret agent trying to crack all the codes to prevent a disaster" << std::endl;
-    std::cout << "There are three numbers in the code" << std::endl;
-
-    while (Level <= 5)
-    {
+int main() {
+    int LDifficulty = 1;
+    int MaxDifficulty = 5;
+    std::cout <<"\nYou are a secret agent" << std::endl; 
+    std::cout <<"\nCrack the codes in the given time" << std::endl;
+    while (LDifficulty <= MaxDifficulty) {
         srand(time(NULL));
-
-        if (PlayGame(Level))
-        {
-            Level++;
-            if (Level <= 5)
-            {
-                std::cout << "Congrats!! Moving on to the next level" << std::endl;
-            }
-            else
-            {
-                std::cout << "You have finished the game!" << std::endl;
-            }
+        if (PlayGame(LDifficulty)) {
+            LDifficulty++;
         }
     }
-
-    return 0;
+    std::cout << "\nGood job dumbass! You finished the game!" << std::endl;
 }
 
-bool PlayGame(int LevelDifficulty)
-{
+bool PlayGame(int difficulty) {
     //Declaring Variables
-    int CodeA = rand() % 4 + LevelDifficulty;
-    int CodeB = rand() % 4 + LevelDifficulty;
-    int CodeC = rand() % 4 + LevelDifficulty;
+    int CodeA = rand() % 2 +1;
+    int CodeB = rand() % 4 +1;
+    int CodeC = rand() % 6 +1;
 
-    int const CodeSum = CodeA + CodeB + CodeC;
-    int const CodeProduct = CodeA * CodeB * CodeC;
-
-    std::cout << "Level:" << LevelDifficulty << std::endl; 
-    std::cout << "\nThe product of the code is: " << CodeProduct;
-    std::cout << "\nThe sum of the code is: " << CodeSum;
-    std::cout << "\nPlease enter the code:" << std::endl;
-
-    //Declaring Guess
+    int CodeSum = CodeA + CodeB + CodeC; 
+    int CodeProduct = CodeA * CodeB * CodeC;
     int GuessA, GuessB, GuessC;
 
-    int GuessSum;
-    int GuessProduct;
-
-    //Waiting for player input
-    std::cin >> GuessA >> GuessB >> GuessC;
-
-    GuessSum = GuessA + GuessB + GuessC;
-    GuessProduct = GuessA * GuessB * GuessC;
-
-    if (GuessSum == CodeSum
-        && GuessProduct == CodeProduct)
-    {
+    //Print Introduction
+    std::cout << "\nLevel: " << difficulty << std::endl;
+    std::cout << "The sum of the code is: " << CodeSum << std::endl;
+    std::cout << "The product of the code is: " << CodeProduct << std::endl;
+    //Waiting for player guess
+    std::cout << "Please enter the first code: ";
+    std::cin >> GuessA;
+    std::cout << "Please enter the second code: ";
+    std::cin >> GuessB;
+    std::cout << "Please enter the third code: ";
+    std::cin >> GuessC;
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
+        std::cout << "You found the right code!" << std::endl;
         return true;
+    } else {
+        std::cout << "Try again!" << std::endl;
+        return false;
     }
-    std::cout << "Try again! :(" << std::endl;
-    return false;
 }
